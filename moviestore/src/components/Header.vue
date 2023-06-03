9f0a6d96fb0646fa02dbde780fb731dede8c377b
John Zhang <zhangjohn.ca@gmail.com>
2023-06-03 13:54:18 -0400

    initialized repo, vue, pinia, axios, and file imports

---
+++ moviestore/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div id="header-container">
+    <h2>Welcome to moviestore</h2>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ moviestore/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
+import Footer from "../components/Footer.vue";
+import axios from "axios";
+import { useStore } from "../stores/index.js";
+
+</script>
+
+<template>
+  <div id="home-container">
+    <Header />
+    <Hero />
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
